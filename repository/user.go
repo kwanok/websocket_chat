122a8df3a7d4dad8f5240d92028d4c9c35b02093
nohkwanok <cloq@kakao.com>
2021-12-29 22:26:37 +0900

    안쓰는 함수 삭제

--- repository/user.go
+++ repository/user.go
@@ -2,9 +2,7 @@ package repository
 
 import (
 	"database/sql"
-	"friday/config"
 	"friday/config/auth"
-	"friday/config/utils"
 	"friday/models"
 	"github.com/google/uuid"
 	"log"
@@ -164,57 +162,4 @@ func (repo *UserRepository) GetAllClients() []models.Client {
 	}
 
 	return users
-}
-
-func GetAllUser() ([]User, error) {
-	users := make([]User, 0)
-
-	rows, err := config.DBCon.Query("SELECT id, level, name, email, created_at, updated_at FROM users")
-	utils.FatalError{Error: err}.Handle()
-	defer rows.Close()
-
-	for rows.Next() {
-		var user User
-		err := rows.Scan(&user.Id, &user.Level, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
-		utils.FatalError{Error: err}.Handle()
-		users = append(users, user)
-	}
-
-	return users, nil
-}
-
-func GetUserByEmail(email string) (User, error) {
-	var user User
-
-	query := "SELECT id, level, name, email, password, created_at, updated_at FROM users WHERE email = ? LIMIT 1"
-
-	err := config.DBCon.QueryRow(query, email).Scan(&user.Id, &user.Level, &user.Name, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
-	utils.FatalError{Error: err}.Handle()
-
-	return user, nil
-}
-
-func GetUserById(id string) (User, error) {
-	var user User
-
-	query := "SELECT id, level, name, email, password, created_at, updated_at FROM users WHERE id = ? LIMIT 1"
-
-	err := config.DBCon.QueryRow(query, id).Scan(&user.Id, &user.Level, &user.Name, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
-	utils.FatalError{Error: err}.Handle()
-
-	return user, nil
-}
-
-func CreateUser(email string, level int, password string, name string) error {
-	tx, err := config.DBCon.Begin()
-	utils.PanicError{Error: err}.Handle()
-	defer tx.Rollback()
-
-	_, err = tx.Exec("INSERT INTO users (name, level, email, password) VALUES (?, ?, ?, ?)", name, level, email, auth.Hash(password))
-	utils.PanicError{Error: err}.Handle()
-
-	err = tx.Commit()
-	utils.PanicError{Error: err}.Handle()
-
-	return nil
 }
